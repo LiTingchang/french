2cfce1637c179ab3e23a16942356c3a3f3573613
LiTingchang <tingchanglee@gmail.com>
2015-12-28 22:27:24 +0800

    fixbugs

---
+++ app/app/src/main/java/com/snail/french/activity/ReportActivity.java
@@ -0,0 +1,16 @@
+package com.snail.french.activity;
+
+import android.os.Bundle;
+
+import com.snail.french.activity.base.BaseActivity;
+
+/**
+ * Created by litingchang on 15-12-9.
+ */
+public class ReportActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
